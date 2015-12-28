7ee46b6d55ecaea2f2ade57892181b0ad1f7ff93
Shmulik <s.chicvashvili@gmail.com>
2015-12-28 20:54:12 +0200

    Starting the project

---
+++ src/Utils/Coordinate.java
@@ -0,0 +1,30 @@
+package Utils;
+
+/**
+ * Created by Shmulik on 12/28/2015.
+ */
+public class Coordinate {
+    private double latitude;
+    private double longitude;
+
+    public Coordinate(double latitude, double longtitude) {
+        this.latitude = latitude;
+        this.longitude = longtitude;
+    }
+
+    public double getLatitude() {
+        return latitude;
+    }
+
+    public void setLatitude(double latitude) {
+        this.latitude = latitude;
+    }
+
+    public double getLongitude() {
+        return longitude;
+    }
+
+    public void setLongitude(double longitude) {
+        this.longitude = longitude;
+    }
+}
---
+++ src/bounding_box/FindBoundingBox.java
@@ -0,0 +1,13 @@
+package bounding_box;
+
+/**
+ * Created by Shmulik on 12/28/2015.
+ */
+public class FindBoundingBox {
+    public FindBoundingBox() {
+        // For now I don't see a reason why this class should hold anything
+        // I'll invoke static methods which will be executing the algorithm correctly
+    }
+
+
+}
